f10c170855e59fc769e48578ff286a5ef33f3ce9
Francesco Ciocchetti <primeroznl@gmail.com>
2017-07-20 22:30:02 +0100

    add cryptonote-universal-pool

---
+++ configs/pool/config-web.js
@@ -0,0 +1,19 @@
+var api = "https://xmr.v9n.us/pool";
+
+var coinUnits = 1000000000000;
+
+var poolHost = "xmr.v9n.us";
+
+var irc = "irc.freenode.net/#monero-pools";
+
+var email = "poolxmr@protonmail.com";
+
+var cryptonatorWidget = ["XMR-BTC", "XMR-USD", "XMR-EUR"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
+
+var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+
+var themeCss = "themes/motherboard-dark-theme.css";
---
+++ cryptonote-universal-pool/runpool.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sleep 10
+cd /cryptonote-universal-pool
+source /root/.nvm/nvm.sh 
+nvm use 0.10.25
+node init.js
+
